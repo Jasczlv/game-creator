8a2635c6ac45394c28c3965aa7879db5f3fd863f
Jasczlv <ianis.cozlov@gmail.com>
2024-06-04 16:12:23 +0200

    ADD create.blade fix

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -14,35 +14,33 @@
 
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome</label>
-                        <input type="text" name="name" class="form-control" id="name"
-                            placeholder="Titolo del fumetto">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Nome">
                     </div>
 
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrizione</label>
                         <input type="text" name="description" class="form-control" id="description"
-                            placeholder="Titolo del fumetto">
+                            placeholder="Descrizione">
                     </div>
 
                     <div class="mb-3">
                         <label for="attack" class="form-label">Attacco</label>
-                        <input type="text" name="attack" class="form-control" id="attack"
-                            placeholder="Titolo del fumetto">
+                        <input type="text" name="attack" class="form-control" id="attack" placeholder="max 40">
                     </div>
 
                     <div class="mb-3">
                         <label for="defence" class="form-label">Defence</label>
-                        <input type="text" name="defence" class="form-control" id="defence" placeholder="http://...">
+                        <input type="text" name="defence" class="form-control" id="defence" placeholder="max 40">
                     </div>
 
                     <div class="mb-3">
                         <label for="speed" class="form-label">Velocita'</label>
-                        <input type="text" name="speed" class="form-control" id="speed" placeholder="novel book">
+                        <input type="text" name="speed" class="form-control" id="speed" placeholder="max 40">
                     </div>
 
                     <div class="mb-3">
                         <label for="life" class="form-label">Punti HP</label>
-                        <input type="text" name="life" class="form-control" id="life" placeholder="Name">
+                        <input type="text" name="life" class="form-control" id="life" placeholder="max 40">
                     </div>
 
 
