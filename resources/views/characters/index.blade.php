daf4b1dc4be976ddf8028da5519675d9ea3ca1d6
hajjamn <hajjamn@gmail.com>
2024-06-04 16:19:43 +0200

    ADD: added anchors to character.shows

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -20,7 +20,7 @@
                     @foreach ($characters as $character)
                         <tr>
 
-                            <td>{{ $character->name }}</td>
+                            <td><a href="{{ route('characters.show',$character) }}">{{ $character->name }}</a></td>
                             <td>{{ $character->description }}</td>
                             <td>{{ $character->attack }}</td>
                             <td>{{ $character->defence }}</td>
