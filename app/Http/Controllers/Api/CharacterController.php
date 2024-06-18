860ca18dc9ccf2f74302851000f90c25bccb7d5d
MattiaBologna <mattiabologna01@gmail.com>
2024-06-18 15:32:37 +0200

    Add: Api Routes index and show and Api/CharacterController

---
+++ app/Http/Controllers/Api/CharacterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Character;
+use Illuminate\Http\Request;
+
+class CharacterController extends Controller
+{
+    public function index(Request $request)
+    {
+        $per_page = $request->perPage ?? 10;
+        $characters = Character::with('type', 'weapons')->paginate($per_page);
+
+        return response()->json([
+            'success' => true,
+            'characters' => $characters
+        ]);
+    }
+}
